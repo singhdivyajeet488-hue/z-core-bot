8ac9f718061ce99e1f0960d72bbc35b73792bc35
singhdivyajeet488hue- <singhdivyajeet488-hue@users.noreply.github.com>
2026-06-09 07:26:57 +0530

    fix port

--- src/handlers/dashboard.js
+++ src/handlers/dashboard.js
@@ -1,5 +1,6 @@
 module.exports = (client) => {
-  const app = require('express')();
+  const express = require('express');
+  const app = express();
   const port = process.env.PORT || 3000;
 
   app.get('/', (req, res) => {
